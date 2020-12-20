9c6d2eb5f84c13b762932f0cf404ce56adcd6feb
Renee <rmui10@stuy.edu>
2020-12-19 19:24:37 -0500

    Fix SQLite thread error (hopefully)

--- app/db_builder.py
+++ app/db_builder.py
@@ -1,37 +1,49 @@
 import sqlite3
 
 DB_FILE = "data.db"
-db = sqlite3.connect(DB_FILE)
-c = db.cursor()
 
-c.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT,
+def createTables():
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    c.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT,
             password TEXT, blogname TEXT, blogdescription TEXT);""")
-c.execute("""CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY,
+    c.execute("""CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY,
             userID INTEGER, time DATETIME, title TEXT, post TEXT);""")
+    db.commit()
+    db.close()
+
 
 def register(username, password, blogname, blogdescription):
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
     command = "INSERT INTO users (username, password, blogname, blogdescription) VALUES ('"
     command += username + "','" + password + "','" + blogname + "','" + blogdescription + "');"
     c.execute(command)
+    db.commit()
+    db.close()
 
 def checkUsername(username):
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
     for row in c.execute("SELECT * FROM users;"):
         if (username == row[1]):
             return True
     return False
+    db.commit()
+    db.close()
 
 def printDatabase():
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
     print("--------Users Table-----------")
     for row in c.execute("SELECT * FROM users;"):
         print (row)
+    db.commit()
+    db.close()
 
+createTables()
 register("user1", "pass", "blog", "description")
 print("Should print true: " + str(checkUsername("user1")))
 print("Should print false: " + str(checkUsername("user2")))
 register("user2", "pass1", "blog1", "description1")
 printDatabase()
-
-
-
-#db.commit()
-db.close()
