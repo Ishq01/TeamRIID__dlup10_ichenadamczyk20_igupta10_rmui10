d43b00c7a63256495885c25f1867a40bb84027c1
Ian Chen-Adam <ichenadamczyk20@stuy.edu>
2021-01-20 22:44:12 +0000

    FINISHED REFACTORING! :DDD

--- app/db_builder.py
+++ app/db_builder.py
@@ -1,74 +1,177 @@
 import sqlite3
 import hashlib
+import sys
 
 DB_FILE = "data.db"
 text_factory = str
 salt = b"I am a static, plaintext salt!!@#T gp127 They're actually more effective than one might think..."
 
+from datetime import datetime
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+from werkzeug.security import generate_password_hash
+
+def get_db():
+  #this block of code returns the same connection so it connects with the same db file
+  #for multiple users
+    if 'db' not in g:
+        # creates database with the path set in __init__.py
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.text_factory = text_factory
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+def close_db(e=None):
+    # returns the database if there is a database in g, otherwise returns sets db to None
+    db = g.pop('db', None)
+    # if there was a database stored in g, the database is closed
+    if db is not None:
+        db.close()
+
+#Reads the sql file and executes the commands in it
+def init_db():
+    db = get_db()
+    # runs the commands in schema.sql which is a sql file
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+# makes it so that you can run command 'flask init-db' from the command line
+# and it initializes the database
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    # clears the existing data and create new tables.
+    init_db()
+    # prints to command line that the database is initialized
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    # enables init-db command
+    app.cli.add_command(init_db_command)
+
+
 # salts and hashes the given string
 def saltString(string, salt):
     return hashlib.pbkdf2_hmac('sha256', string.encode('utf-8'), salt, 100000)
 
+# helper function to format list of entries in paged format
+def pageEntries(entries, pageSize):
+    pagedEntries = []
+    # get a range of entries of size pageSize and append to pagedEntries
+    # if the rest of the entries list is too short, appends only the rest of the entries list
+    for i in range(0, len(entries), pageSize):
+        pagedEntries += [entries[i:min(i + pageSize, len(entries))]]
+    return pagedEntries
+
+# helper function to limit characters
+# session["error_msg"] = "character limit exceeded"
+def validateInput(name, value, error_msg_output):
+    # value is stripped of whitespace and name specifies the type of checks to run on value
+    # validateInput runs the checks on value and appends error messages to error_msg
+    # if there were error messages, the stripped value is returned and error_msg is appended to error_msg_output
+    # otherwise the stripped value is returned
+    error_msg = []
+    value = value.strip()
+    if name == "username":
+        if value == "":
+            error_msg += ["Username can not be blank or have only spaces"]
+        if not value.replace("_", "").isalnum() or " " in value:
+            error_msg += ["Username can have only letters, numbers, and underscores"]
+        if auth_methods["checkUsername"](value):
+            error_msg += ["Username already exists"]
+        if len(value) > 100:
+            error_msg += ["Username can have only 100 characters or fewer"]
+
+    if name == "password":
+        if len(value) < 8 or len(value) > 100:
+            error_msg += ["Password must have between 8 and 100 characters"]
+
+    if name == "blogname":
+        if value == "":
+            error_msg += ["Blog name can not be blank or have only spaces"]
+        if len(value) > 100:
+            error_msg += ["Blog name can have only 100 characters or fewer"]
+
+    if name == "blogdescription":
+        if len(value) > 250:
+            error_msg += ["Blog description can have only 250 characters or fewer"]
+
+    if name == "entrytitle":
+        if value == "":
+            error_msg += ["Entry title can not be blank or have only spaces"]
+        if len(value) > 100:
+            error_msg += ["Entry title can have only 100 characters or fewer"]
+
+    if name == "entrypic":
+        if sys.getsizeof(value) > 100000000:
+            error_msg += ["Entry image can be only 100MB or smaller"]
+
+    if name == "entrycontent":
+        if value == "":
+            error_msg += ["Entry content can not be blank or have only spaces"]
+        if len(value) > 10000:
+            error_msg += ["Entry content can have only 10000 characters or fewer"]
+
+    if len(error_msg):
+        error_msg_output += error_msg
+        return value
+    else:
+        return value
+
 # makes users and entries table in database if they do not exist already
 def createTables():
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    c = db.cursor()
-    c.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT,
+    db = get_db()
+    db.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT,
               password TEXT, blogname TEXT, blogdescription TEXT, time DATETIME);""") #
-    c.execute("""CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY,
+    db.execute("""CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY,
               userID INTEGER, time DATETIME, title TEXT, post TEXT, pic TEXT);""")
-    c.execute('CREATE TABLE IF NOT EXISTS followers (userID INTEGER, followerID INTEGER);')
+    db.execute('CREATE TABLE IF NOT EXISTS followers (userID INTEGER, followerID INTEGER);')
     db.commit()
-    db.close()
+
+salt = "plain text salt.. hi. salting is a little different with the refactoring, but it still works i think"
 
 # adds user info to user table
 def register(username, password, blogname, blogdescription):
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    c = db.cursor()
+    db = get_db()
     #Finds the current date and time based on the local time
-    dateAndTimeTup = c.execute("SELECT datetime('now','localtime');").fetchone()
+    dateAndTimeTup = db.execute("SELECT datetime('now','localtime');").fetchone()
     dateAndTime = str(''.join(map(str, dateAndTimeTup)))
     command = "INSERT INTO users (username, password, blogname, blogdescription, time) VALUES (?,?,?,?,?);"
-    c.execute(command, (username, password, blogname, blogdescription, dateAndTime))
+    password = generate_password_hash(password + salt)
+    db.execute(command, (username, password, blogname, blogdescription, dateAndTime))
     db.commit()
-    db.close()
 
 # returns whether or not username is in user table
 def checkUsername(username):
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    c = db.cursor()
+    db = get_db()
     found = False
-    for row in c.execute("SELECT * FROM users;"):
+    for row in db.execute("SELECT * FROM users;"):
         found = found or (username == row[1])
     db.commit()
-    db.close()
     return found
 
 # returns information about a user from the specified column
 # col can be 'id', 'password', 'blogname', or 'blogdescription'
 def getInfo(username, col):
     if checkUsername(username):
-        db = sqlite3.connect(DB_FILE)
-        db.text_factory = text_factory
-        c = db.cursor()
+        db = get_db()
         #Finds the user with the correct username
-        info = c.execute("SELECT " + col + " FROM users WHERE username=?;", [username]).fetchone()[0]
+        info = db.execute("SELECT " + col + " FROM users WHERE username=?;", [username]).fetchone()[0]
         db.commit()
-        db.close()
         return info
     return None
 
 #Gets a username based on a user id
 def getUsername(userID):
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    c = db.cursor()
-    info = c.execute("SELECT username FROM users WHERE id=?;", [userID]).fetchone()
+    db = get_db()
+    info = db.execute("SELECT username FROM users WHERE id=?;", [userID]).fetchone()
     db.commit()
-    db.close()
     if info is None:
         return info
     return info[0]
@@ -76,13 +179,10 @@ def getUsername(userID):
 # changes a user's blog info given a new blog name and description
 def updateBlogInfo(username, blogname, desc):
     if checkUsername(username):
-        db = sqlite3.connect(DB_FILE)
-        db.text_factory = text_factory
-        c = db.cursor()
-        c.execute("UPDATE users SET blogname=? WHERE username=?;", (blogname, username))
-        c.execute("UPDATE users SET blogdescription=? WHERE username=?;", (desc, username))
+        db = get_db()
+        db.execute("UPDATE users SET blogname=? WHERE username=?;", (blogname, username))
+        db.execute("UPDATE users SET blogdescription=? WHERE username=?;", (desc, username))
         db.commit()
-        db.close()
 
 # converts rows in database to a dictionary
 def dict_factory(cursor, row):
@@ -93,84 +193,61 @@ def dict_factory(cursor, row):
 
 # returns a list of dictionaries containing each blog's info
 def getBlogs():
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    db.row_factory = dict_factory
-    c = db.cursor()
-    blogs = c.execute("SELECT * from users ORDER BY time DESC;").fetchall()
+    db = get_db()
+    blogs = db.execute("SELECT * from users ORDER BY time DESC;").fetchall()
     db.commit()
-    db.close()
-    return blogs
+    return [dict(i) for i in blogs]
 
 #Adds an entry to the entries table
 def addEntry(userID, title, post, pic):
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    c = db.cursor()
+    db = get_db()
     #Gets the current date and time
-    dateAndTimeTup = c.execute("SELECT datetime('now','localtime');").fetchone()
+    dateAndTimeTup = db.execute("SELECT datetime('now','localtime');").fetchone()
     dateAndTime = str(''.join(map(str, dateAndTimeTup)))
     command = "INSERT INTO entries (userID, time, title, post, pic) VALUES (?,?,?,?,?);"
     #Executes command
-    c.execute(command, (str(userID), dateAndTime, title, post, pic))
+    db.execute(command, (str(userID), dateAndTime, title, post, pic))
     #New time of entry
-    c.execute("UPDATE users SET time=? WHERE id=?;", (dateAndTime, str(userID)))
+    db.execute("UPDATE users SET time=? WHERE id=?;", (dateAndTime, str(userID)))
     db.commit()
-    db.close()
 
 #Edits a past entry based on id
 def editEntry(entryID, title, post, pic):
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    c = db.cursor()
-    dateAndTimeTup = c.execute("SELECT datetime('now','localtime');").fetchone()
+    db = get_db()
+    dateAndTimeTup = db.execute("SELECT datetime('now','localtime');").fetchone()
     dateAndTime = str(''.join(map(str, dateAndTimeTup)))
     #Updates the entries
-    c.execute("UPDATE entries SET title=? WHERE id=?;", (title, str(entryID)))
-    c.execute("UPDATE entries SET post=? WHERE id=?;", (post, str(entryID)))
-    c.execute("UPDATE entries SET time=? WHERE id=?;", (dateAndTime, str(entryID)))
-    c.execute("UPDATE entries SET pic=? WHERE id=?;", (pic, str(entryID)))
-    userID = c.execute("SELECT userID FROM entries WHERE id=?;", [str(entryID)]).fetchone()
-    c.execute("UPDATE users SET time=? WHERE id=?;", (dateAndTime, str(userID[0])))
+    db.execute("UPDATE entries SET title=? WHERE id=?;", (title, str(entryID)))
+    db.execute("UPDATE entries SET post=? WHERE id=?;", (post, str(entryID)))
+    db.execute("UPDATE entries SET time=? WHERE id=?;", (dateAndTime, str(entryID)))
+    db.execute("UPDATE entries SET pic=? WHERE id=?;", (pic, str(entryID)))
+    userID = db.execute("SELECT userID FROM entries WHERE id=?;", [str(entryID)]).fetchone()
+    db.execute("UPDATE users SET time=? WHERE id=?;", (dateAndTime, str(userID[0])))
     db.commit()
-    db.close()
 
 #Gets all of a users entries
 def getEntries(userID):
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    db.row_factory = dict_factory
-    c = db.cursor()
-    entries = c.execute("SELECT * FROM entries WHERE userID=? ORDER BY time DESC;", [str(userID)]).fetchall()
+    db = get_db()
+    entries = db.execute("SELECT * FROM entries WHERE userID=? ORDER BY time DESC;", [str(userID)]).fetchall()
     db.commit()
-    db.close()
-    return entries
+    return [dict(i) for i in entries]
 
 #Gets an entries information based on entryID
 def getEntryInfo(entryID, col):
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    c = db.cursor()
-    info = c.execute("SELECT " + col + " FROM entries WHERE id=?;", [str(entryID)]).fetchone()[0]
+    db = get_db()
+    info = db.execute("SELECT " + col + " FROM entries WHERE id=?;", [str(entryID)]).fetchone()[0]
     db.commit()
-    db.close()
     return info
 
 #deletes an entry
 def deleteEntry(entryID):
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    c = db.cursor()
-    c.execute("DELETE FROM entries WHERE id=?;", [str(entryID)])
+    db = get_db()
+    db.execute("DELETE FROM entries WHERE id=?;", [str(entryID)])
     db.commit()
-    db.close()
 
 #searches the database's entries for a specific word
 def search(criteria):
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    db.row_factory = dict_factory
-    c = db.cursor()
+    db = get_db()
     criteria_list = ['%' + i.replace('%', '[%]') + '%' for i in criteria.split()]
     command = "SELECT * FROM entries WHERE (post LIKE ?"
     for x in criteria_list[1:]:
@@ -179,58 +256,44 @@ def search(criteria):
     for x in criteria_list[1:]:
         command += " AND title LIKE ?"
     command += ");"
-    entries = c.execute(command, criteria_list + criteria_list).fetchall()
+    entries = db.execute(command, criteria_list + criteria_list).fetchall()
     db.commit()
-    db.close()
-    return entries
+    return [dict(i) for i in entries]
   
 
 # adds row to followers table if it doesn't already exist
 # users with followerID follws user with userID
 def addFollower(userID, followerID):
     if not checkFollower(userID, followerID):
-        db = sqlite3.connect(DB_FILE)
-        db.text_factory = text_factory
-        c = db.cursor()
+        db = get_db()
         command = "INSERT INTO followers VALUES (?,?);"
-        c.execute(command, (userID, followerID))
+        db.execute(command, (userID, followerID))
         db.commit()
-        db.close()
 
 # removes row with specified info from followers table
 def removeFollower(userID, followerID):
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    c = db.cursor()
-    c.execute("DELETE FROM followers WHERE userID=? AND followerID=?;", (str(userID), str(followerID)))
+    db = get_db()
+    db.execute("DELETE FROM followers WHERE userID=? AND followerID=?;", (str(userID), str(followerID)))
     db.commit()
-    db.close()
 
 # return whether or not a user-follower pair exists
 def checkFollower(userID, followerID):
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    c = db.cursor()
-    found = c.execute("SELECT * FROM followers WHERE userID=? AND followerID=?;",
+    db = get_db()
+    found = db.execute("SELECT * FROM followers WHERE userID=? AND followerID=?;",
                       (str(userID), str(followerID))).fetchone()
     db.commit()
-    db.close()
     return found is not None
 
 # returns a list of dictionaries of blogs a user is following
 def getFollowedBlogs(followerID):
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    db.row_factory = dict_factory
-    c = db.cursor()
-    followedUsers = c.execute("SELECT userID FROM followers WHERE followerID=?;", [str(followerID)]).fetchall()
+    db = get_db()
+    followedUsers = db.execute("SELECT userID FROM followers WHERE followerID=?;", [str(followerID)]).fetchall()
     blogs = []
     for user in followedUsers:
-        blog = c.execute("SELECT * FROM users WHERE id=?;", [str(user["userID"])]).fetchone()
+        blog = db.execute("SELECT * FROM users WHERE id=?;", [str(user["userID"])]).fetchone()
         blogs.append(blog)
     db.commit()
-    db.close()
-    return blogs
+    return [dict(i) for i in blogs]
 
 #Clears everything
 def clearAll():
@@ -239,68 +302,51 @@ def clearAll():
     clearFollowers()
 
 def clear():
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    c = db.cursor()
-    c.execute("DROP TABLE entries;")
+    db = get_db()
+    db.execute("DROP TABLE entries;")
     db.commit()
-    db.close()
 
 # deletes all users from the database (for testing purposes)
 def clearUsers():
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    c = db.cursor()
-    c.execute("DELETE from users;")
+    db = get_db()
+    db.execute("DELETE from users;")
     db.commit()
-    db.close()
 
 # Delete a specific user
 def clearUser(username):
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    c = db.cursor()
-    c.execute("DELETE from users WHERE username=?;", [username])
+    db = get_db()
+    db.execute("DELETE from users WHERE username=?;", [username])
     db.commit()
-    db.close()
 
 # deletes everything in followers table
 def clearFollowers():
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    c = db.cursor()
-    c.execute("DELETE from followers;")
+    db = get_db()
+    db.execute("DELETE from followers;")
     db.commit()
-    db.close()
 
 #Clears all entries, bugfixxing
 def clearEntries():
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    c = db.cursor()
-    c.execute("DELETE from entries;")
+    db = get_db()
+    db.execute("DELETE from entries;")
     db.commit()
-    db.close()
 
 # prints user table (for testing purposes)
 def printDatabase():
-    db = sqlite3.connect(DB_FILE)
-    db.text_factory = text_factory
-    c = db.cursor()
+    db = get_db()
     print("--------Users Table-----------")
-    for row in c.execute("SELECT * FROM users;"):
-        print(row)
+    for row in db.execute("SELECT * FROM users;"):
+        print(list(row))
     print("-------Entries Table----------")
-    for row in c.execute("SELECT * FROM entries;"):
-        print(row)
+    for row in db.execute("SELECT * FROM entries;"):
+        print(list(row))
     db.commit()
-    db.close()
 
 #create a dictionary of method name: method for auth methods
 auth_methods = {
     'addUser': register,
     'checkUsername': checkUsername,
-    'getPwd': getInfo
+    'getPwd': getInfo, 
+    'salt': salt
 }
 
 #create a dictionary of method name: method for blog/entry methods 
